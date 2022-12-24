fe6ca661f68f0ab5d2d043ea5c42fa58b8571005
gioZAK <giovane.zakc@gmail.com>
2022-12-24 10:38:52 +0000

    Fix the error caused by deleting important imports

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,11 +1,15 @@
+
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
+from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import View
 from django.contrib.auth.models import User
+from blog.models import Post, Comment
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .forms import ProfileForm
 from .models import Profile
-from blog.models import Post, Comment
+
 
 
 # Receiver created in order to create a profile for the user after register
